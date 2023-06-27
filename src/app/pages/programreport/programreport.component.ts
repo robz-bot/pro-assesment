abfa24bf9d0c21cdf3bd41331dc1d5f8b3fdfafd
Vishalrajz <127098556+Vishalrajz@users.noreply.github.com>
2023-06-27 14:44:20 +0530

    report
    final module with report

--- src/app/pages/programreport/programreport.component.ts
+++ src/app/pages/programreport/programreport.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { report } from '../assessment/report';
 import { ReportService } from '../report/report.service';
 import { Router } from '@angular/router';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-programreport',
@@ -22,6 +23,8 @@ export class ProgramreportComponent implements OnInit {
 
   }
   reportList: any = [];
+  excelReportList: { reportedOn: string, teamName: string, userName: string, scoredMark: number, totalMark: number }[] = [
+  ];
   todayDate: any
   request: report = new report();
   reportDate: any
@@ -34,12 +37,85 @@ export class ProgramreportComponent implements OnInit {
         console.log(data);
         this.reportList = data;
 
-        const uniqueRecords = [...new Set(this.reportList.map((obj: any) => JSON.stringify({ reportedOn: obj.reportedOn, teamName: obj.teamName, userName: obj.userName, userId: obj.userId })))].map((str: any) => JSON.parse(str));
-        console.log(uniqueRecords);
+        // const uniqueRecords = [...new Set(this.reportList.map((obj: any) => JSON.stringify({ reportedOn: obj.reportedOn, teamName: obj.teamName, userName: obj.userName, userId: obj.userId })))].map((str: any) => JSON.parse(str));
+        // console.log(uniqueRecords);
 
-        this.reportList = uniqueRecords
+        // this.reportList = uniqueRecords
+
+        // Create a Map to store the unique records
+        const uniqueRecords = new Map();
+
+        // Iterate through each object
+        for (const obj of this.reportList) {
+          obj.totalMark = parseInt(obj.totalMark);
+          obj.scoredMark = parseInt(obj.scoredMark);
+          // Create a unique key based on reportedOn, teamName, userName, and userId
+          const key = `${obj.reportedOn}-${obj.teamName}-${obj.userName}-${obj.userId}`;
+
+          // If the key already exists in the Map, add the scoredMark and totalMark values to the existing record
+          if (uniqueRecords.has(key)) {
+            const record = uniqueRecords.get(key);
+            record.scoredMark += parseInt(obj.scoredMark);
+            record.totalMark += parseInt(obj.totalMark);
+          } else {
+            // If the key doesn't exist, create a new record and add it to the Map
+            uniqueRecords.set(key, {
+              reportedOn: obj.reportedOn,
+              teamName: obj.teamName,
+              userName: obj.userName,
+              userId: obj.userId,
+              scoredMark: obj.scoredMark,
+              totalMark: obj.totalMark,
+              status:Number.isNaN(obj.scoredMark)?"Yet to  review": "Reviewed"
+            });
+          }
+        }
+
+        // Convert the Map values to an array of unique records
+        const uniqueRecordsArray = Array.from(uniqueRecords.values());
+
+        console.log(uniqueRecordsArray);
+        this.reportList = uniqueRecordsArray
+        this.excelReportList = this.reportList
       }
     )
+  }
+
+  // Method to export table data to Excel
+  exportToExcel(): void {
+
+    // Rearrange the list and remove the userId property
+    const rearrangedList = this.excelReportList.map(({ reportedOn, teamName, userName, scoredMark, totalMark }) => ({
+      "Full Name": userName,
+      "Team": teamName,
+      "Scored Marks": scoredMark,
+      "Total Marks": totalMark,
+      "Reported On": reportedOn
+    }));
+
+    // Create an empty workbook and worksheet
+    const workbook: XLSX.WorkBook = XLSX.utils.book_new();
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(rearrangedList);
+
+    // Add the worksheet to the workbook
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+    // Generate Excel file binary data
+    const excelData: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+
+    // Create a Blob object from the Excel data
+    const blob = new Blob([excelData], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+
+    // Create a download link element
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'level2_report.xlsx';
+    link.click();
+
+    // Clean up
+    window.URL.revokeObjectURL(url);
+    link.remove();
   }
 
   gotoDatewisereport(item: any) {
